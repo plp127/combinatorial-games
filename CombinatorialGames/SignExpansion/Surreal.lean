5695ee5e32f7f53c2e797e0fb0515921f1fea192
Aaron Liu <aaronliu2008@outlook.com>
2025-12-31 15:30:57 -0500

    rename to `truncate`

--- CombinatorialGames/SignExpansion/Surreal.lean
+++ CombinatorialGames/SignExpansion/Surreal.lean
@@ -20,13 +20,13 @@ noncomputable section
 namespace Surreal
 open IGame SignExpansion
 
-private def signApproxIGame (o : NatOrdinal.{u}) (x : Surreal.{u}) : IGame.{u} :=
+private def truncIGame (o : NatOrdinal.{u}) (x : Surreal.{u}) : IGame.{u} :=
   !{{s : IGame.{u} | s.birthday < o} ∩ {s | ∃ _ : s.Numeric, .mk s < x} |
     {s : IGame.{u} | s.birthday < o} ∩ {s | ∃ _ : s.Numeric, x < .mk s}}
 
-private instance signApproxIGameNumeric (o : NatOrdinal.{u}) (x : Surreal.{u}) :
-    Numeric (x.signApproxIGame o) := by
-  rw [numeric_def, signApproxIGame]
+private local instance truncIGameNumeric (o : NatOrdinal.{u}) (x : Surreal.{u}) :
+    Numeric (x.truncIGame o) := by
+  rw [numeric_def, truncIGame]
   refine ⟨?_, Player.rec ?_ ?_⟩
   · intro y hy z hz
     rw [leftMoves_ofSets] at hy
@@ -41,34 +41,34 @@ private instance signApproxIGameNumeric (o : NatOrdinal.{u}) (x : Surreal.{u}) :
     rw [rightMoves_ofSets] at hy
     exact hy.2.1
 
-def signApprox (o : NatOrdinal.{u}) (x : Surreal.{u}) : Surreal.{u} :=
-  mk (x.signApproxIGame o)
+def truncate (o : NatOrdinal.{u}) (x : Surreal.{u}) : Surreal.{u} :=
+  mk (x.truncIGame o)
 
-theorem birthday_signApprox_le (o : NatOrdinal.{u}) (x : Surreal.{u}) :
-    (signApprox o x).birthday ≤ o := by
-  rw [signApprox]
+theorem birthday_truncate_le (o : NatOrdinal.{u}) (x : Surreal.{u}) :
+    (truncate o x).birthday ≤ o := by
+  rw [truncate]
   apply (birthday_mk_le _).trans
-  rw [signApproxIGame, birthday_le_iff, leftMoves_ofSets, rightMoves_ofSets]
+  rw [truncIGame, birthday_le_iff, leftMoves_ofSets, rightMoves_ofSets]
   constructor
   · intro y hy
     exact hy.left
   · intro y hy
     exact hy.left
 
-theorem signApprox_of_birthday_le {o : NatOrdinal.{u}} {x : Surreal.{u}}
-    (h : x.birthday ≤ o) : x.signApprox o = x := by
+theorem truncate_of_birthday_le {o : NatOrdinal.{u}} {x : Surreal.{u}}
+    (h : x.birthday ≤ o) : x.truncate o = x := by
   obtain ⟨k, nk, rfl, hk⟩ := x.birthday_eq_iGameBirthday
   rw [← hk] at h
-  rw [signApprox, Surreal.mk_eq_mk]
+  rw [truncate, Surreal.mk_eq_mk]
   symm
   apply Fits.equiv_of_forall_birthday_le
   · constructor
     · intro z hz
-      rw [signApproxIGame, leftMoves_ofSets] at hz
+      rw [truncIGame, leftMoves_ofSets] at hz
       obtain ⟨hz, nz, hzk⟩ := hz
       rwa [← not_le, mk_le_mk] at hzk
     · intro z hz
-      rw [signApproxIGame, rightMoves_ofSets] at hz
+      rw [truncIGame, rightMoves_ofSets] at hz
       obtain ⟨hz, nz, hkz⟩ := hz
       rwa [← not_le, mk_le_mk] at hkz
   · intro z nz hz
@@ -80,23 +80,23 @@ theorem signApprox_of_birthday_le {o : NatOrdinal.{u}} {x : Surreal.{u}}
       exact not_le_of_gt hbb (birthday_mk_le z)
     obtain hzk | hkz := lt_or_gt_of_ne hne
     · refine hz.left z ?_ le_rfl
-      rw [signApproxIGame, leftMoves_ofSets]
+      rw [truncIGame, leftMoves_ofSets]
       exact ⟨hbb.trans_le h, nz, hzk⟩
     · refine hz.right z ?_ le_rfl
-      rw [signApproxIGame, rightMoves_ofSets]
+      rw [truncIGame, rightMoves_ofSets]
       exact ⟨hbb.trans_le h, nz, hkz⟩
 
 @[simp]
-theorem signApprox_eq_iff {x : Surreal.{u}} {o : NatOrdinal.{u}} :
-    x.signApprox o = x ↔ x.birthday ≤ o := by
-  refine ⟨fun h => ?_, signApprox_of_birthday_le⟩
+theorem truncate_eq_iff {x : Surreal.{u}} {o : NatOrdinal.{u}} :
+    x.truncate o = x ↔ x.birthday ≤ o := by
+  refine ⟨fun h => ?_, truncate_of_birthday_le⟩
   refine le_of_not_gt fun ho => ?_
   refine ne_of_lt ?_ (congrArg birthday h)
-  exact (x.birthday_signApprox_le o).trans_lt ho
+  exact (x.birthday_truncate_le o).trans_lt ho
 
-theorem monotone_signApprox {o : NatOrdinal.{u}} : Monotone (signApprox o) := by
+theorem monotone_truncate {o : NatOrdinal.{u}} : Monotone (truncate o) := by
   intro x y hxy
-  simp_rw [signApprox, Surreal.mk_le_mk, signApproxIGame]
+  simp_rw [truncate, Surreal.mk_le_mk, truncIGame]
   rw [le_iff_forall_lf]
   refine ⟨fun z hz => left_lf ?_, fun z hz => lf_right ?_⟩
   · rw [leftMoves_ofSets] at hz ⊢
@@ -105,11 +105,11 @@ theorem monotone_signApprox {o : NatOrdinal.{u}} : Monotone (signApprox o) := by
     exact ⟨hz.1, hz.2.1, lt_of_le_of_lt hxy hz.2.2⟩
 
 def ofSurreal (x : Surreal.{u}) : SignExpansion where
-  sign i := .sign (x - x.signApprox i)
+  sign i := .sign (x - x.truncate i)
   isUpperSet_preimage_singleton_zero' := by
     intro a b hab ha
     rw [Set.mem_preimage, Set.mem_singleton_iff,
-      sign_eq_zero_iff, sub_eq_zero, eq_comm, signApprox_eq_iff] at ha ⊢
+      sign_eq_zero_iff, sub_eq_zero, eq_comm, truncate_eq_iff] at ha ⊢
     exact ha.trans hab
 
 @[simp]
@@ -121,9 +121,9 @@ theorem size_ofSurreal (x : Surreal.{u}) :
   | top => simp
   | coe c =>
     rw [← apply_eq_zero, WithTop.coe_le_coe, ofSurreal, coe_mk,
-      sign_eq_zero_iff, sub_eq_zero, eq_comm, signApprox_eq_iff]
+      sign_eq_zero_iff, sub_eq_zero, eq_comm, truncate_eq_iff]
 
 theorem ofSurreal_apply (x : Surreal.{u}) (o : NatOrdinal.{u}) :
-    ofSurreal x o = .sign (x - x.signApprox o) := rfl
+    ofSurreal x o = .sign (x - x.truncate o) := rfl
 
 end Surreal
