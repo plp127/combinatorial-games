eb0579925e4d0d10b830399d9fd988a07088b5e0
Aaron Liu <aaronliu2008@outlook.com>
2025-12-31 16:08:45 -0500

    inline

--- CombinatorialGames/SignExpansion/Surreal.lean
+++ CombinatorialGames/SignExpansion/Surreal.lean
@@ -20,66 +20,50 @@ noncomputable section
 namespace Surreal
 open IGame SignExpansion
 
-private def truncIGame (o : NatOrdinal.{u}) (x : Surreal.{u}) : IGame.{u} :=
-  !{{s : IGame.{u} | s.birthday < o} ∩ {s | ∃ _ : s.Numeric, .mk s < x} |
-    {s : IGame.{u} | s.birthday < o} ∩ {s | ∃ _ : s.Numeric, x < .mk s}}
-
-private local instance truncIGameNumeric (o : NatOrdinal.{u}) (x : Surreal.{u}) :
-    Numeric (x.truncIGame o) := by
-  rw [numeric_def, truncIGame]
-  refine ⟨?_, Player.rec ?_ ?_⟩
-  · intro y hy z hz
-    rw [leftMoves_ofSets] at hy
-    rw [rightMoves_ofSets] at hz
-    have := hy.2.1
-    have := hz.2.1
-    exact mk_lt_mk.mp (hy.2.2.trans hz.2.2)
-  · intro y hy
-    rw [leftMoves_ofSets] at hy
-    exact hy.2.1
-  · intro y hy
-    rw [rightMoves_ofSets] at hy
-    exact hy.2.1
-
 def truncate (o : NatOrdinal.{u}) : Surreal.{u} →o Surreal.{u} where
-  toFun x := mk (x.truncIGame o)
+  toFun x := @mk !{fun p => {s : IGame.{u} | s.birthday < o} ∩
+    {s | ∃ _ : s.Numeric, p.cases (.mk s < x) (x < .mk s)}} <| by
+      rw [numeric_def]
+      refine ⟨fun y hy z hz => ?_, fun p y hy => ?_⟩
+      · rw [moves_ofSets] at hy hz
+        exact (@mk_lt_mk y z hy.2.1 hz.2.1).mp (hy.2.2.trans hz.2.2)
+      · rw [moves_ofSets] at hy
+        exact hy.2.1
   monotone' := by
     intro x y hxy
-    simp_rw [Surreal.mk_le_mk, truncIGame]
+    simp_rw [Surreal.mk_le_mk]
     rw [le_iff_forall_lf]
-    refine ⟨fun z hz => left_lf ?_, fun z hz => lf_right ?_⟩
-    · rw [leftMoves_ofSets] at hz ⊢
-      exact ⟨hz.1, hz.2.1, lt_of_lt_of_le hz.2.2 hxy⟩
-    · rw [rightMoves_ofSets] at hz ⊢
-      exact ⟨hz.1, hz.2.1, lt_of_le_of_lt hxy hz.2.2⟩
+    refine ⟨fun z hz => left_lf ?_, fun z hz => lf_right ?_⟩ <;> rw [moves_ofSets] at hz ⊢
+    · exact ⟨hz.1, hz.2.1, lt_of_lt_of_le hz.2.2 hxy⟩
+    · exact ⟨hz.1, hz.2.1, lt_of_le_of_lt hxy hz.2.2⟩
+
+theorem truncate_apply (o : NatOrdinal.{u}) (x : Surreal.{u}) :
+    ∃ h : Numeric _, truncate o x = @mk !{fun p => {s : IGame.{u} | s.birthday < o} ∩
+      {s | ∃ _ : s.Numeric, p.cases (.mk s < x) (x < .mk s)}} h := ⟨_, rfl⟩
 
 theorem birthday_truncate_le (o : NatOrdinal.{u}) (x : Surreal.{u}) :
     (truncate o x).birthday ≤ o := by
-  rw [truncate]
+  obtain ⟨_, h⟩ := truncate_apply o x
+  rw [h]
   apply (birthday_mk_le _).trans
-  rw [truncIGame, birthday_le_iff, leftMoves_ofSets, rightMoves_ofSets]
-  constructor
-  · intro y hy
-    exact hy.left
-  · intro y hy
-    exact hy.left
+  rw [birthday_le_iff, ← @Player.forall fun p => ∀ y ∈ moves p _, y.birthday < o]
+  intro p y hy
+  rw [moves_ofSets] at hy
+  exact hy.1
 
 theorem truncate_of_birthday_le {o : NatOrdinal.{u}} {x : Surreal.{u}}
     (h : x.birthday ≤ o) : x.truncate o = x := by
   obtain ⟨k, nk, rfl, hk⟩ := x.birthday_eq_iGameBirthday
   rw [← hk] at h
-  rw [truncate, OrderHom.coe_mk, Surreal.mk_eq_mk]
+  obtain ⟨_, ht⟩ := truncate_apply o (mk k)
+  rw [ht, Surreal.mk_eq_mk]
   symm
   apply Fits.equiv_of_forall_birthday_le
-  · constructor
-    · intro z hz
-      rw [truncIGame, leftMoves_ofSets] at hz
-      obtain ⟨hz, nz, hzk⟩ := hz
-      rwa [← not_le, mk_le_mk] at hzk
-    · intro z hz
-      rw [truncIGame, rightMoves_ofSets] at hz
-      obtain ⟨hz, nz, hkz⟩ := hz
-      rwa [← not_le, mk_le_mk] at hkz
+  · constructor <;>
+      · intro z hz
+        rw [moves_ofSets] at hz
+        obtain ⟨-, _, hz⟩ := hz
+        simpa using hz
   · intro z nz hz
     apply le_of_not_gt
     intro hbb
@@ -89,10 +73,10 @@ theorem truncate_of_birthday_le {o : NatOrdinal.{u}} {x : Surreal.{u}}
       exact not_le_of_gt hbb (birthday_mk_le z)
     obtain hzk | hkz := lt_or_gt_of_ne hne
     · refine hz.left z ?_ le_rfl
-      rw [truncIGame, leftMoves_ofSets]
+      rw [moves_ofSets]
       exact ⟨hbb.trans_le h, nz, hzk⟩
     · refine hz.right z ?_ le_rfl
-      rw [truncIGame, rightMoves_ofSets]
+      rw [moves_ofSets]
       exact ⟨hbb.trans_le h, nz, hkz⟩
 
 @[simp]
