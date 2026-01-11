7dc8450841b3c584941fad867a661124305936e0
Aaron Liu <aaronliu2008@outlook.com>
2026-01-11 13:36:24 -0500

    add multiplication file

---
+++ CombinatorialGames/Surreal/Multiplication.lean
@@ -0,0 +1,13 @@
+/-
+Copyright (c) 2026 Aaron Liu. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Aaron Liu
+-/
+import CombinatorialGames.Surreal.Basic
+
+universe u
+
+namespace Surreal.Multiplication
+open IGame
+
+end Surreal.Multiplication
