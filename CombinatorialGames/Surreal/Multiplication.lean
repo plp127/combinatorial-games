b8fde5448137fbfa4aff8426821252bae50c73b7
Aaron Liu <aaronliu2008@outlook.com>
2026-01-12 22:40:08 -0500

    add defs for relation and sorried theorem

--- CombinatorialGames/Surreal/Multiplication.lean
+++ CombinatorialGames/Surreal/Multiplication.lean
@@ -10,4 +10,17 @@ universe u
 namespace Surreal.Multiplication
 open IGame
 
+def Rel2 (a b : IGame × IGame) : Prop :=
+  ((a.1 = b.1 ∨ Subposition a.1 b.1) ∨ (a.1 = b.2 ∨ Subposition a.1 b.2)) ∧
+  (Subposition a.2 b.1 ∨ Subposition a.2 b.2) ∨
+  (Subposition a.1 b.1 ∨ Subposition a.1 b.2) ∧
+  ((a.2 = b.1 ∨ Subposition a.2 b.1) ∨ (a.2 = b.2 ∨ Subposition a.2 b.2))
+
+def WRel2 (a b : IGame × IGame) : Prop :=
+  ((a.1 = b.1 ∨ Subposition a.1 b.1) ∨ (a.1 = b.2 ∨ Subposition a.1 b.2)) ∧
+  ((a.2 = b.1 ∨ Subposition a.2 b.1) ∨ (a.2 = b.2 ∨ Subposition a.2 b.2))
+
+theorem rel2_wf : WellFounded Rel2 := by
+  sorry
+
 end Surreal.Multiplication
